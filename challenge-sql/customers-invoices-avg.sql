5168f509560fbcc519c4187a6fffc8bc4f8ee452
smarjane <marjane.selman@ynov.com>
2026-01-19 21:33:04 +0100

    e32

--- challenge-sql/customers-invoices-avg.sql
+++ challenge-sql/customers-invoices-avg.sql
@@ -1,2 +1,5 @@
-SELECT c.FirstName, c.LastName, AVG(i.Total) AS InvoicesAverage FROM customers c
-INNER JOIN invoices i ON c.CustomerId = i.CustomerId GROUP BY c.CustomerId ORDER BY c.FirstName ASC
+SELECT c.FirstName, c.LastName, AVG(i.Total) AS InvoicesAverage
+FROM customers c
+INNER JOIN invoices i ON c.CustomerId = i.CustomerId 
+GROUP BY c.CustomerId 
+ORDER BY c.FirstName ASC;
